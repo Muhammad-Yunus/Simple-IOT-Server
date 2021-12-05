dd7d5908da1566a4b9196127a09eaf284e194a73
Muhammad Yunus <34908065+Muhammad-Yunus@users.noreply.github.com>
2021-12-05 11:35:12 +0700

    Update environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,3 +1,11 @@
+echo "Installing Python"
+sudo apt install python3 -y
+sudo apt install python3-venv -y
+sudo apt install python3-pip -y
+
+echo "Creating Environment"
 python3 -m venv env 
 source env/bin/activate
+
+echo "Installing Dependency Library"
 pip3 install -r requirements.txt
